2fb394479ba17a59bceedceadadb044a83c4f471
Rahul Malhan <36480669+RahulMalhan@users.noreply.github.com>
2019-02-20 14:21:52 +0530

    Create crypto.cpp

---
+++ crypto.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void encrypt(char* file , char* file1 , int key)
+{
+	fstream cin(file , ios::in);
+	string s;
+	getline(cin , s);
+	for(int j = 0 ; j < s.size() ; j++)
+	{
+		s[j] = (s[j] + key) % 256;
+	}
+	cin.close();
+	fstream cout(file1 , ios::out);
+	cout  << s;
+	cout.close();
+}
+
+void decrypt(char* file , char* file1 , int key)
+{
+	fstream cin(file1 , ios::in);
+	string s;
+	getline(cin , s);
+	for(int j = 0 ; j < s.size() ; j++)
+	{
+		if((s[j] - key) < 0)
+		{
+			s[j] = s[j] - key + 256;
+		}
+		else
+			s[j] = (s[j] - key) % 256;
+	}
+	cin.close();
+	fstream cout(file , ios::out);
+	cout << s;
+	cout.close();
+}
+
+int main(int argc , char** argv)
+{	
+	string s = argv[2];
+	int key;
+	cout << "enter the key for encryption: " << endl;
+	cin >> key;
+	if(s == "-e")
+	{
+		encrypt(argv[3] , argv[4] , key);
+	}
+	if(s == "-d")
+	{
+		cout << "enter key value: " << endl;
+		int key1;
+		cin >> key1;
+		if(key1 == key)
+			decrypt(argv[3] , argv[4] , key);
+		else
+			cout << "key value entered is wrong!";
+	}
+	return 0;
+}
